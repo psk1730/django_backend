def2b95b48598685a11d33bb33be20969af77ad3
Pravesh Kumar <pravesh.psk@gmail.com>
2025-08-01 14:19:11 +0530

    finalised all things

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 SECRET_KEY = 'django-insecure-o=*=--)7#(7=@0lp^--xg13t#gw_&qp#*a%-8c-%m$vnv8)%4g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.0.2.2', 'localhost','127.0.0.1', '192.168.132.235']
 
